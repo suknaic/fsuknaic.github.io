3a551ad7d888276bfae54a17ba5f9e379771253e
Felipe Suknaic <fsuknaic@hotmail.com>
2025-08-12 01:07:48 -0500

    🔧 Refatorar configuração do Next.js

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     use: ["@svgr/webpack"],
+  //   });
+  //   return config;
+  // },
   output: "export",
   reactStrictMode: true,
   // Para GitHub Pages - Como o repositório é fsuknaic.github.io, não precisamos de basePath
